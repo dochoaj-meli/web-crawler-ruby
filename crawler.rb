76a5af0cc3c8bd6a3df12dfb991f769ddf338e05
Daniel Ochoa <daniel.ochoa@mercadolibre.cl>
2021-01-10 22:59:55 -0300

    Crawl url and output unique results

--- crawler.rb
+++ crawler.rb
@@ -1,14 +1,12 @@
-require 'wombat'
+require 'mechanize'
 
 class Crawler
   def perform(url)
-    Wombat.crawl do # De acá se usa la gema Wombat (https://github.com/felipecsl/wombat)
-      base_url url # Marca el parametro url como la URL base
-      path "/" # Y usa la raíz para navegar
-
-      # Trae todos los links en forma de lista. 
-      # Retorna un hashmap con la llave "links" y en el valor un array de los textos de los links.
-      links({ xpath: "//a" }, :list) 
-    end
+    agent = Mechanize.new
+    page = agent.get(url)
+    page.search('//a').map do |link|
+      href = link.attr('href')
+      href.include?('http') ? href : "#{url}#{href[1..]}"
+    end.uniq.compact
   end
 end
